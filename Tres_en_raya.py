0e68590e22cef3ea8f0b4ccca2f12af260c6e7d7
Daniel-Iglesias-Carballal <danieliglesiascarballal@gmail.com>
2026-05-25 12:06:41 +0200

    YM3-1: Funcionalidad basica

---
+++ Tres_en_raya.py
@@ -0,0 +1,27 @@
+print("Tres en raya")
+
+tablero = [" " for _ in range(9)]
+
+
+def mostrar_tablero():
+    print()
+    print(f" {tablero[0]} | {tablero[1]} | {tablero[2]} ")
+    print("---+---+---")
+    print(f" {tablero[3]} | {tablero[4]} | {tablero[5]} ")
+    print("---+---+---")
+    print(f" {tablero[6]} | {tablero[7]} | {tablero[8]} ")
+    print()
+
+
+def comprobar_ganador(jugador):
+    combinaciones = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
