e9c32d78e11339cfa8ef22a2373c9c1f05485957
Daniel-Iglesias-Carballal <danieliglesiascarballal@gmail.com>
2026-05-25 12:10:59 +0200

    YM3-1: Desarrollo funcionalidad basica

--- Tres_en_raya.py
+++ Tres_en_raya.py
@@ -1,27 +1,47 @@
-print("Tres en raya")
 
-tablero = [" " for _ in range(9)]
+tablero = ["1", "2", "3",
+           "4", "5", "6",
+           "7", "8", "9"]
 
-
-def mostrar_tablero():
-    print()
-    print(f" {tablero[0]} | {tablero[1]} | {tablero[2]} ")
-    print("---+---+---")
-    print(f" {tablero[3]} | {tablero[4]} | {tablero[5]} ")
-    print("---+---+---")
-    print(f" {tablero[6]} | {tablero[7]} | {tablero[8]} ")
-    print()
+turno = "X"
 
 
-def comprobar_ganador(jugador):
-    combinaciones = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+def mostrar():
+    print(tablero[0], "|", tablero[1], "|", tablero[2])
+    print("--+---+--")
+    print(tablero[3], "|", tablero[4], "|", tablero[5])
+    print("--+---+--")
+    print(tablero[6], "|", tablero[7], "|", tablero[8])
 
+
+while True:
+    mostrar()
+
+    pos = int(input("Elige posición (1-9): ")) - 1
+
+    if tablero[pos] == "X" or tablero[pos] == "O":
+        print("Casilla ocupada")
+        continue
+
+    tablero[pos] = turno
+
+    # Comprobar victoria
+    if (
+        (tablero[0] == tablero[1] == tablero[2]) or
+        (tablero[3] == tablero[4] == tablero[5]) or
+        (tablero[6] == tablero[7] == tablero[8]) or
+        (tablero[0] == tablero[3] == tablero[6]) or
+        (tablero[1] == tablero[4] == tablero[7]) or
+        (tablero[2] == tablero[5] == tablero[8]) or
+        (tablero[0] == tablero[4] == tablero[8]) or
+        (tablero[2] == tablero[4] == tablero[6])
+    ):
+        mostrar()
+        print("Gana", turno)
+        break
+
+    # Cambiar turno
+    if turno == "X":
+        turno = "O"
+    else:
+        turno = "X"
