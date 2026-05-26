eee4068537d91bcadc124c7e43903e421c4db863
Daniel-Iglesias-Carballal <danieliglesiascarballal@gmail.com>
2026-05-26 12:19:26 +0200

    YM3-3: Mejora de visualización del tablero

--- Tres_en_raya.py
+++ Tres_en_raya.py
@@ -7,41 +7,42 @@ turno = "X"
 
 
 def mostrar():
-    print(tablero[0], "|", tablero[1], "|", tablero[2])
-    print("--+---+--")
-    print(tablero[3], "|", tablero[4], "|", tablero[5])
-    print("--+---+--")
-    print(tablero[6], "|", tablero[7], "|", tablero[8])
-
+    print("\n╔═══╦═══╦═══╗")
+    print(f"║ {tablero[0]} ║ {tablero[1]} ║ {tablero[2]} ║")
+    print("╠═══╬═══╬═══╣")
+    print(f"║ {tablero[3]} ║ {tablero[4]} ║ {tablero[5]} ║")
+    print("╠═══╬═══╬═══╣")
+    print(f"║ {tablero[6]} ║ {tablero[7]} ║ {tablero[8]} ║")
+    print("╚═══╩═══╩═══╝\n")
 
 while True:
-    mostrar()
+        mostrar()
 
-    pos = int(input("Elige posición (1-9): ")) - 1
 
-    if tablero[pos] == "X" or tablero[pos] == "O":
-        print("Casilla ocupada")
-        continue
+        pos = int(input("Elige posición (1-9): ")) - 1
 
-    tablero[pos] = turno
+        if tablero[pos] == "X" or tablero[pos] == "O":
+            print("Casilla ocupada")
+            continue
+        tablero[pos] = turno
 
     # Comprobar victoria
-    if (
-        (tablero[0] == tablero[1] == tablero[2]) or
-        (tablero[3] == tablero[4] == tablero[5]) or
-        (tablero[6] == tablero[7] == tablero[8]) or
-        (tablero[0] == tablero[3] == tablero[6]) or
-        (tablero[1] == tablero[4] == tablero[7]) or
-        (tablero[2] == tablero[5] == tablero[8]) or
-        (tablero[0] == tablero[4] == tablero[8]) or
-        (tablero[2] == tablero[4] == tablero[6])
-    ):
-        mostrar()
-        print("Gana", turno)
-        break
+        if (
+                (tablero[0] == tablero[1] == tablero[2]) or
+                (tablero[3] == tablero[4] == tablero[5]) or
+                (tablero[6] == tablero[7] == tablero[8]) or
+                (tablero[0] == tablero[3] == tablero[6]) or
+                (tablero[1] == tablero[4] == tablero[7]) or
+                (tablero[2] == tablero[5] == tablero[8]) or
+                (tablero[0] == tablero[4] == tablero[8]) or
+                (tablero[2] == tablero[4] == tablero[6])
+        ):
+                mostrar()
+                print("¡Gana",turno,"!")
+                break
 
-    # Cambiar turno
-    if turno == "X":
-        turno = "O"
-    else:
-        turno = "X"
+        # Cambiar turno
+        if turno == "X":
+            turno = "O"
+        else:
+            turno = "X"
